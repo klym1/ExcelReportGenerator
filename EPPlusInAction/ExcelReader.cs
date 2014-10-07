9ba868ea1e49a886c2caa12e9283b441eb45d638
Mykola Klymyuk <mykola.klymyuk@gmail.com>
2014-10-08 02:24:12 +0300

    Integrated Costura.Fody For Single Exe

--- EPPlusInAction/ExcelReader.cs
+++ EPPlusInAction/ExcelReader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ExcelReportGenerator
@@ -21,33 +22,41 @@ namespace ExcelReportGenerator
         {
             var t = new MyExcelReader();
 
-            var g =  t.Page_Load(_filePath);
-            
-            var list = new List<RecordRaw1>();
-
-            for (int i = 0; i < g.GetLength(0); i++)
+            try
             {
-                var row = new RecordRaw1
+                var g = t.Page_Load(_filePath);
+
+                var list = new List<RecordRaw1>();
+
+                for (int i = 0; i < g.GetLength(0); i++)
                 {
-                    sys_acs_cd = g[i, 0],
-                    cst_nm = g[i, 1],
-                    cst_nam = g[i, 2],
-                    bsns_srvc_nm = g[i, 3],
-                    bsns_trn_long_nam = g[i, 4],
-                    bsns_srvc_prcs_dt = g[i, 5],
-                    plcy_nm = g[i, 6],
-                    plt_cd = g[i, 7],
-                    iss_stck_ref_nm = g[i, 8],
-                    bsns_srvc_sum_amt = g[i, 9]
+                    var row = new RecordRaw1
+                    {
+                        sys_acs_cd = g[i, 0],
+                        cst_nm = g[i, 1],
+                        cst_nam = g[i, 2],
+                        bsns_srvc_nm = g[i, 3],
+                        bsns_trn_long_nam = g[i, 4],
+                        bsns_srvc_prcs_dt = g[i, 5],
+                        plcy_nm = g[i, 6],
+                        plt_cd = g[i, 7],
+                        iss_stck_ref_nm = g[i, 8],
+                        bsns_srvc_sum_amt = g[i, 9]
+                    };
+
+                    list.Add(row);
+                }
+
+                return new MonthModel(list)
+                {
+                    Name = Path.GetFileNameWithoutExtension(_filePath)
                 };
-                
-                list.Add(row);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Input file was in incorrect format!");
             }
             
-            return new MonthModel(list)
-            {
-                Name = Path.GetFileNameWithoutExtension(_filePath)
-            };
         }
     }
 }
